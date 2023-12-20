3c127fe4917b2ad00f8097dd02198957df7346c6
Daniel Padrino <danswarrior1@gmail.com>
2023-12-20 11:08:25 -0300

    deploy

--- src/hooks/useUser.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchUser } from "../utils/apis";
-
-export const useUser = () => {
-  return useQuery({
-    queryKey: [`user-preferences`],
-    queryFn: () => fetchUser(),
-  });
-};
